dcba66115b164b1ac7c093604ae7a60296b7d8a3
viktak <github@viktak.com>
2020-05-29 12:38:58 +0300

    wifi connect fix

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,6 @@ bool needsTemperature = false;
 
 //  Other global variables
 config appConfig;
-char timeout = 30;
 bool isAccessPoint = false;
 bool isAccessPointCreated = false;
 
@@ -1162,14 +1161,14 @@ void loop(){
           // Initialize iteration counter
           char attempt = 0;
 
-          while ((WiFi.status() != WL_CONNECTED) && (attempt++ < timeout)) {
+          while ((WiFi.status() != WL_CONNECTED) && (attempt++ < WIFI_CONNECTION_TIMEOUT)) {
             digitalWrite(CONNECTION_STATUS_LED_GPIO, LOW);
             Serial.print(".");
             delay(50);
             digitalWrite(CONNECTION_STATUS_LED_GPIO, HIGH);
             delay(950);
           }
-          if (attempt > WIFI_CONNECTION_TIMEOUT) {
+          if (attempt >= WIFI_CONNECTION_TIMEOUT) {
             Serial.println();
             Serial.println("Could not connect to WiFi");
             delay(100);
--- tools/LittleFS/LittleFSBuilder.py
+++
@@ -1,4 +0,0 @@
-Import("env")
-#env.Replace( MKSPIFFSTOOL='python" "' + env.get("PROJECT_DIR") + '/mklittlefs.py' )
-env.Replace (MKSPIFFSTOOL = env.get ("PROJECT_DIR") + "/tools/LittleFs/mklittlefs.exe")
-#env.Replace( MKSPIFFSTOOL='python" "' + env.get('PROJECT_DIR') + '/mklittlefs.py' )
